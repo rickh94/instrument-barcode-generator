6bdb02840edf8452949bf46aba71eba5da63b690
Rick Henry <fredericmhenry@gmail.com>
2019-05-22 13:44:26 -0400

    switch to svg and testing
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- generate.py
+++ generate.py
@@ -9,19 +9,23 @@ from pathlib import Path
 def generate_single_barcode(data):
     print(data)
     CODE128 = barcode.get_barcode_class("code128")
-    code = CODE128(data, writer=ImageWriter())
-    with Path("codes", f"{data}.png".replace('/', '-')).open("wb") as f:
+    # code = CODE128(data, writer=ImageWriter())
+    code = CODE128(data)
+    with Path("codes", f"{data}.svg".replace("/", "-")).open("wb") as f:
         code.write(f)
 
+
 def get_all_instrument_numbers():
-    at = Airtable(base_key=os.environ.get('AIRTABLE_BASE_KEY'), table_name='Instruments')
+    at = Airtable(
+        base_key=os.environ.get("AIRTABLE_BASE_KEY"), table_name="Instruments"
+    )
     for page in at.get_iter():
         for record in page:
-            if not record['fields'].get('Number', False):
+            if not record["fields"].get("Number", False):
                 continue
-            generate_single_barcode(record['fields']['Number'].strip())
+            generate_single_barcode(record["fields"]["Number"].strip())
+            return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     get_all_instrument_numbers()
-
